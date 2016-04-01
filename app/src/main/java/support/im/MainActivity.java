2cfa03d2eda506b7df1607f20bb7d93e59235ad1
杨玉刚 <smartydroid@gmail.com>
2016-04-01 18:04:17 +0800

    First Commit

---
+++ app/src/main/java/support/im/MainActivity.java
@@ -0,0 +1,89 @@
+package support.im;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import com.ncapdevi.fragnav.FragNavController;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarBadge;
+import com.roughike.bottombar.OnMenuTabClickListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends SupportImActivity implements SupportImFragment.FragmentNavigation {
+
+  //Better convention to properly name the indices what they are in your app
+  private final int INDEX_CONVERSIONS = FragNavController.TAB1;
+  private final int INDEX_CONTACTS = FragNavController.TAB2;
+  private final int INDEX_ACCOUNTS = FragNavController.TAB3;
+  private final int INDEX_ACCOUNTS2 = FragNavController.TAB2;
+
+  private BottomBar mBottomBar;
+  private FragNavController mNavController;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    setupFragNav();
+    setupBottomBar(savedInstanceState);
+  }
+
+  @Override protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+
+    // Necessary to restore the BottomBar's state, otherwise we would
+    // lose the current tab on orientation change.
+    mBottomBar.onSaveInstanceState(outState);
+  }
+
+  private void setupFragNav() {
+    List<Fragment> fragments = new ArrayList<>(5);
+    fragments.add(ConversationsFragment.newInstance());
+    fragments.add(ContactsFragment.newInstance());
+    fragments.add(AccountsFragment.newInstance());
+    mNavController = new FragNavController(getSupportFragmentManager(), R.id.container, fragments);
+  }
+
+  private void setupBottomBar(Bundle savedInstanceState) {
+    mBottomBar = BottomBar.attach(this, savedInstanceState);
+    mBottomBar.setItemsFromMenu(R.menu.menu_bottombar, new OnMenuTabClickListener() {
+      @Override public void onMenuTabSelected(@IdRes int menuItemId) {
+        switch (menuItemId) {
+          case R.id.menu_conversation:
+            mNavController.switchTab(INDEX_CONVERSIONS);
+            break;
+          case R.id.menu_contact:
+            mNavController.switchTab(INDEX_CONTACTS);
+            break;
+          case R.id.menu_accounts:
+            mNavController.switchTab(INDEX_ACCOUNTS);
+            break;
+        }
+      }
+
+      @Override public void onMenuTabReSelected(@IdRes int menuItemId) {
+        mNavController.clearStack();
+      }
+    });
+
+    // Make a Badge for the first tab, with red background color and a value of "13".
+    BottomBarBadge unreadMessages = mBottomBar.makeBadgeForTabAt(INDEX_CONVERSIONS, "#FF0000", 13);
+    // Control the badge's visibility
+    unreadMessages.show();
+
+    //mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+  }
+
+  @Override public void onBackPressed() {
+    if (mNavController.getCurrentStack().size() > 1) {
+      mNavController.pop();
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override public void pushFragment(Fragment fragment) {
+    mNavController.push(fragment);
+  }
+}
---
+++ app/src/main/java/support/im/SupportImFragment.java
@@ -0,0 +1,53 @@
+package support.im;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.ButterKnife;
+
+/**
+ * Created by YuGang Yang on 04 01, 2016.
+ * Copyright 20015-2016 honc.tech. All rights reserved.
+ */
+public abstract class SupportImFragment extends Fragment {
+
+  FragmentNavigation mFragmentNavigation;
+
+  @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(getFragmentLayout(), container, false);
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ButterKnife.bind(this, view);
+  }
+
+  @Override public void onAttach(Context context) {
+    super.onAttach(context);
+    if (context instanceof FragmentNavigation) {
+      mFragmentNavigation = (FragmentNavigation) context;
+    }
+  }
+
+  @Override public void onDetach() {
+    super.onDetach();
+    mFragmentNavigation = null;
+  }
+
+  /**
+   * Every fragment has to inflate a layout in the onCreateView method. We have added this method
+   * to
+   * avoid duplicate all the inflate code in every fragment. You only have to return the layout to
+   * inflate in this method when extends StarterFragment.
+   */
+  protected abstract int getFragmentLayout();
+
+  public interface FragmentNavigation {
+    void pushFragment(Fragment fragment);
+  }
+}
