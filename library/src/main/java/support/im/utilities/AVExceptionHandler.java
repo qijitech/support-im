5fdd28dd655108e5e9743f42e50b711368af83dc
杨玉刚 <smartydroid@gmail.com>
2016-04-26 17:43:33 +0800

    fix

--- library/src/main/java/support/im/utilities/AVExceptionHandler.java
+++ library/src/main/java/support/im/utilities/AVExceptionHandler.java
@@ -8,7 +8,7 @@ public class AVExceptionHandler {
 
   public static boolean handAVException(Context context, AVException e) {
     if (e != null) {
-      Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+      Toast.makeText(context, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
       return false;
     } else {
       return true;
