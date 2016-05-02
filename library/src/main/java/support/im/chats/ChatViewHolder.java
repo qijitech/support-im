06d4b9f586114807e11881a682aadce3d5efbf1c
杨玉刚 <smartydroid@gmail.com>
2016-05-02 14:57:15 +0800

    fix

--- library/src/main/java/support/im/chats/ChatViewHolder.java
+++ library/src/main/java/support/im/chats/ChatViewHolder.java
@@ -3,12 +3,13 @@ package support.im.chats;
 import android.content.Context;
 import android.view.ViewGroup;
 import com.avos.avoscloud.im.v2.AVIMMessage;
+import support.im.R;
 import support.ui.adapters.EasyViewHolder;
 
 public class ChatViewHolder extends EasyViewHolder<AVIMMessage> {
 
   public ChatViewHolder(Context context, ViewGroup parent) {
-    super(context, parent, layoutId);
+    super(context, parent, R.layout.chats_right_item);
   }
 
   @Override public void bindTo(int position, AVIMMessage value) {
