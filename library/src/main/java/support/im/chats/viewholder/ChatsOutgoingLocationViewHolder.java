c0c5b005ea52f77072a757eb7a9054db7457a346
杨玉刚 <smartydroid@gmail.com>
2016-05-13 17:49:51 +0800

    fix chats location style

--- library/src/main/java/support/im/chats/viewholder/ChatsOutgoingLocationViewHolder.java
+++ library/src/main/java/support/im/chats/viewholder/ChatsOutgoingLocationViewHolder.java
@@ -6,15 +6,19 @@ import android.widget.TextView;
 import butterknife.ButterKnife;
 import com.avos.avoscloud.im.v2.AVIMMessage;
 import com.avos.avoscloud.im.v2.messages.AVIMLocationMessage;
+import com.facebook.drawee.view.SimpleDraweeView;
 import support.im.R;
+import support.im.utilities.Utils;
 
 public class ChatsOutgoingLocationViewHolder extends ChatsOutgoingViewHolder {
 
   protected TextView mLocationTextView;
+  protected SimpleDraweeView simpleDraweeView;
 
   public ChatsOutgoingLocationViewHolder(Context context, ViewGroup parent) {
     super(context, parent, R.layout.chats_outgoing_item_location);
     mLocationTextView = ButterKnife.findById(itemView, R.id.text_support_im_chats_item_location);
+    simpleDraweeView = ButterKnife.findById(itemView, R.id.image_support_im_chats_item_location);
   }
 
   @Override public void bindTo(int position, AVIMMessage value) {
@@ -22,6 +26,7 @@ public class ChatsOutgoingLocationViewHolder extends ChatsOutgoingViewHolder {
     if (value instanceof AVIMLocationMessage) {
       final AVIMLocationMessage locMsg = (AVIMLocationMessage) value;
       mLocationTextView.setText(locMsg.getText());
+      simpleDraweeView.setImageURI(Utils.buildStaticMapUrl(locMsg.getLocation()));
     }
   }
 }
