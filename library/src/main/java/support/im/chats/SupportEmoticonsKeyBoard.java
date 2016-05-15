303e0e210e9cfc39de2b85b37ae2a7838a981e58
杨玉刚 <smartydroid@gmail.com>
2016-05-15 21:40:39 +0800

    add perm check

--- library/src/main/java/support/im/chats/SupportEmoticonsKeyBoard.java
+++ library/src/main/java/support/im/chats/SupportEmoticonsKeyBoard.java
@@ -1,8 +1,13 @@
 package support.im.chats;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import de.greenrobot.event.EventBus;
+import pl.tajchert.nammu.Nammu;
+import pl.tajchert.nammu.PermissionCallback;
 import sj.keyboard.XhsEmoticonsKeyBoard;
 import support.im.R;
 import support.im.events.ChatRecordEvent;
@@ -17,6 +22,25 @@ public class SupportEmoticonsKeyBoard extends XhsEmoticonsKeyBoard {
 
   @Override protected void inflateKeyboardBar() {
     mInflater.inflate(R.layout.view_support_keyboard, this);
+  }
+
+  @Override public void onClick(final View v) {
+    if (v.getId() == R.id.btn_voice_or_text && this.mRlInput.isShown()) {
+      if (Nammu.checkPermission(Manifest.permission.RECORD_AUDIO)) {
+        super.onClick(v);
+      } else {
+        Nammu.askForPermission((Activity) getContext(), Manifest.permission.RECORD_AUDIO,
+          new PermissionCallback() {
+            @Override public void permissionGranted() {
+              SupportEmoticonsKeyBoard.super.onClick(v);
+            }
+            @Override public void permissionRefused() {
+            }
+          });
+      }
+      return;
+    }
+    super.onClick(v);
   }
 
   /**
