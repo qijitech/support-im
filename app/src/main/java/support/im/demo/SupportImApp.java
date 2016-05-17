05181c4ae40964fdddbf047c97f700593dba369b
Doublemine <doublemine.w@gmail.com>
2016-05-17 18:24:09 +0800

    fix bug: cause exception on android 4.x

--- app/src/main/java/support/im/demo/SupportImApp.java
+++ app/src/main/java/support/im/demo/SupportImApp.java
@@ -1,5 +1,6 @@
 package support.im.demo;
 
+import android.content.Context;
 import android.support.multidex.MultiDex;
 import com.facebook.stetho.Stetho;
 import support.im.SupportIm;
@@ -8,8 +9,7 @@ import support.im.demo.features.main.MainActivity;
 public class SupportImApp extends support.im.SupportImApp {
 
   @Override public void onCreate() {
-    SupportIm.initialize(BuildConfig.LeanCloundAppId,
-        BuildConfig.LeanCloundAppKey,
+    SupportIm.initialize(BuildConfig.LeanCloundAppId, BuildConfig.LeanCloundAppKey,
         MainActivity.class, BuildConfig.DEBUG);
     super.onCreate();
     MultiDex.install(this);
@@ -18,5 +18,10 @@ public class SupportImApp extends support.im.SupportImApp {
         .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
         .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
         .build());
+  }
+
+  @Override protected void attachBaseContext(Context base) {
+    super.attachBaseContext(base);
+    MultiDex.install(this);
   }
 }
